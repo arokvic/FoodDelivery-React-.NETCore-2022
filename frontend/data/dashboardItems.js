73dfa83c79c9c31e3fe7852660fa68e22e6154f9
Aleksa Rokvic <64163740+arokvic@users.noreply.github.com>
2022-06-11 18:53:17 +0200

    data

---
+++ frontend/data/dashboardItems.js
@@ -0,0 +1,67 @@
+const dashboardItems = [
+  {
+    id: 1,
+    url: '/icon-individual.png',
+    title: 'Edit profile',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+  {
+    id: 2,
+    url: '/6.png',
+    title: 'New order',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+  {
+    id: 3,
+    url: '/static/images/buttons/camera.jpg',
+    title: 'Previous orders',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+  {
+    id: 4,
+    url: '/static/images/buttons/camera.jpg',
+    title: 'Verification',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+  {
+    id: 5,
+    url: '/icon-individual.png',
+    title: 'Pending orders',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+  {
+    id: 6,
+    url: '/6.png',
+    title: 'My orders',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+  {
+    id: 7,
+    url: '/static/images/buttons/camera.jpg',
+    title: 'Curent orders',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+  {
+    id: 8,
+    url: '/static/images/buttons/camera.jpg',
+    title: 'All orders',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+  {
+    id: 9,
+    url: '/static/images/buttons/camera.jpg',
+    title: 'New product',
+    width: '33%',
+    redirectUrl: '/register',
+  },
+];
+
+export default dashboardItems;
